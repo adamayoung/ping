998b5b7784d139a42067452fc9ad48b4a421f416
Adam Young <me@adam-young.co.uk>
2023-10-26 16:32:19 +0100

    Deployment target versions

--- Ping/PingApp.swift
+++ Ping/PingApp.swift
@@ -5,28 +5,13 @@
 //  Created by Adam Young on 26/10/2023.
 //
 
-import SwiftData
 import SwiftUI
 
 @main
 struct PingApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
